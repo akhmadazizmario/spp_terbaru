88bfc0f4f1651dfc1f5346b3d932f3a380f6d6b2
akhmad aziz mario <12201084@bsi.ac.id>
2021-12-04 11:46:12 +0700

    upload terbaru

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -15,7 +15,7 @@
          </div>
          <div class="jumbotron jumbotron-fluid bg-white">
              <div class="container">
-                 <h1 class="display-4">Selamat Datang <?= $user['username']; ?>, <?= $user['id_user']; ?>
+                 <h1 class="display-4">Selamat Datang <?= $user['username']; ?>
                  </h1>
                  <p class="lead">Aplikasi Pembayaran SPP Sekolah Sederhana SMA N TEGAL</p>
              </div>
